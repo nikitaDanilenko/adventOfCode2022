90e488362c96e8471693bc5c962c51142410a9f4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-12-08 15:55:47 +0100

    Add solution for day 8.2

--- src/main/scala/day08/Day08.scala
+++ src/main/scala/day08/Day08.scala
@@ -1,11 +1,16 @@
 package day08
 
+import cats.kernel.Order
+import cats.syntax.order._
+
 import scala.io.Source
-import scala.util.chaining._
+import scala.util.chaining.*
 
 object Day08 {
 
-  val input: Map[(Int, Int), Int] = Source
+  type Forest = Map[(Int, Int), Int]
+
+  val input: Forest = Source
     .fromResource("day08.txt")
     .getLines()
     .zipWithIndex
@@ -16,26 +21,61 @@ object Day08 {
     }
     .toMap
 
-  def computeVisible(map: Map[(Int, Int), Int]): Int =
+  case class Rows(
+      left: List[Int],
+      right: List[Int],
+      top: List[Int],
+      bottom: List[Int]
+  )
+
+  def crossOf(map: Forest, range: Int, pos: (Int, Int)): Rows =
+    val (i, j) = pos
+    Rows(
+      left = 0.until(j).map(index => map((i, index))).toList,
+      right = (1 + j).to(range).map(index => map((i, index))).toList,
+      top = 0.until(i).map(index => map((index, j))).toList,
+      bottom = (1 + i).to(range).map(index => map((index, j))).toList
+    )
+
+  def computeVisible(map: Forest): Int =
     val range = map.keys.map(_._1).max
     map.count { case ((i, j), height) =>
-      val left = 0.until(j).map(index => map((i, index)))
-      val right = (1 + j).to(range).map(index => map((i, index)))
-      val top = 0.until(i).map(index => map((index, j)))
-      val bottom = (1 + i).to(range).map(index => map((index, j)))
+      val cross = crossOf(map, range, (i, j))
 
       List(
-        left,
-        right,
-        top,
-        bottom
+        cross.left,
+        cross.right,
+        cross.top,
+        cross.bottom
       ).exists(_.forall(_ < height))
     }
+
+  def scenicScore(map: Forest, pos: (Int, Int), height: Int): BigInt =
+    val range = map.keys.map(_._1).max
+    val (i, j) = pos
+    val cross = crossOf(map, range, (i, j))
+    List(
+      cross.left.reverse,
+      cross.right,
+      cross.top.reverse,
+      cross.bottom
+    ).map(takeWhileSmallerToAndStop(_, height).length.pipe(BigInt(_))).product
+
+  def takeWhileSmallerToAndStop[A: Order](xs: List[A], max: A): List[A] =
+    val (smaller, greaterOrEqual) = xs.span(_ < max)
+    smaller ++ greaterOrEqual.headOption
 
   @main
   def solution1(): Unit =
     input
       .pipe(computeVisible)
+      .pipe(pprint.log(_))
+
+  @main
+  def solution2(): Unit =
+    input
+      .map { case ((i, j), height) => scenicScore(input, (i, j), height) }
+      .max
       .pipe(pprint.log(_))
 
 }

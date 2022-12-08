7a1bc6ec7761fdcc9ebaceecf9d4e8091114b597
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-12-08 12:26:16 +0100

    Add solution for day 7.1

--- build.sbt
+++ build.sbt
@@ -1,12 +1,15 @@
-val scala3Version = "3.1.3"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.1.3"
 
 lazy val root = (project in file("."))
   .settings(
     name := "adventOfCode2022",
-    version := "0.1.0-SNAPSHOT",
-    scalaVersion := scala3Version,
     libraryDependencies ++= Seq(
-      "com.lihaoyi"   %% "pprint"     % "0.8.1",
-      "org.typelevel" %% "cats-parse" % "0.3.8"
+      "com.lihaoyi"   %% "pprint"         % "0.8.0",
+      "org.typelevel" %% "spire"          % "0.18.0-M2",
+      "dev.optics"    %% "monocle-unsafe" % "3.1.0",
+      "org.typelevel" %% "cats-parse"     % "0.3.8",
+      "org.typelevel" %% "cats-core"      % "2.8.0"
     )
   )
---
+++ src/main/scala/day07/Day07.scala
@@ -0,0 +1,171 @@
+package day07
+
+import cats.parse.Parser
+import utils.ParserUtil
+import scala.util.chaining._
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day07 {
+
+  // File system element
+  sealed trait FSE {
+    def name: String
+  }
+
+  object FSE {
+
+    case class File(
+        override val name: String,
+        size: Int
+    ) extends FSE
+
+    case class Folder(
+        override val name: String,
+        contents: Vector[FSE]
+    ) extends FSE
+
+    val parser: Parser[FSE] = {
+      val fileParser = for {
+        size <- ParserUtil.int
+        _ <- Parser.char(' ')
+        name <- Parser.anyChar.rep0.map(_.mkString)
+      } yield File(
+        name = name,
+        size = size
+      )
+
+      val dirParser = for {
+        _ <- Parser.string("dir ")
+        name <- Parser.anyChar.rep0.map(_.mkString)
+      } yield Folder(name, Vector.empty)
+
+      Parser.oneOf(
+        List(
+          fileParser,
+          dirParser
+        )
+      )
+    }
+
+  }
+
+  def addFSE(current: FSE, path: List[String], toAdd: Vector[FSE]): FSE =
+    current match
+      case file: FSE.File => file
+      case folder @ FSE.Folder(name, contents) =>
+        path match
+          case step :: rest if step == name =>
+            if rest.isEmpty then
+              folder.copy(
+                contents = folder.contents ++ toAdd
+              )
+            else
+              folder.copy(
+                contents = contents.map(addFSE(_, rest, toAdd))
+              )
+          case _ =>
+            folder
+
+  sealed trait Command
+
+  object Command {
+    case class Enter(name: String) extends Command
+    case class Output(fse: FSE) extends Command
+    case object Read extends Command
+    case object Exit extends Command
+
+    def isOutput(command: Command): Boolean =
+      command match
+        case Output(_) => true
+        case _         => false
+
+    val parser: Parser[Command] = {
+      val exitParser = Parser.string("$ cd ..").map(_ => Command.Exit)
+      val enterParser = for {
+        _ <- Parser.string("$ cd ")
+        name <- Parser.anyChar.rep0.map(_.mkString)
+      } yield Command.Enter(name)
+
+      val readParser = Parser.string("$ ls").map(_ => Command.Read)
+      val outputParser = FSE.parser.map(Command.Output.apply)
+
+      Parser.oneOf(
+        List(
+          exitParser,
+          enterParser,
+          readParser,
+          outputParser
+        )
+      )
+    }
+
+  }
+
+  def addViaCommands(commands: List[Command]): FSE = {
+    val initial = FSE.Folder("/", Vector.empty)
+
+    @tailrec
+    def search(commands: List[Command], currentPath: List[String], fse: FSE): FSE =
+      commands match
+        case head :: next =>
+          head match
+            case Command.Enter(name) => search(next, currentPath :+ name, fse)
+            case Command.Output(fse) =>
+              // Should be subsumed by the Command.Read action
+              search(next, currentPath, fse)
+            case Command.Read =>
+              val (outputs, rest) = next.span(Command.isOutput)
+              search(
+                commands = rest,
+                currentPath = currentPath,
+                fse = addFSE(
+                  fse,
+                  currentPath,
+                  outputs.collect { case Command.Output(fse) => fse }.toVector
+                )
+              )
+            case Command.Exit =>
+              search(next, currentPath.dropRight(1), fse)
+        case Nil => fse
+
+    search(commands, currentPath = List.empty, initial)
+  }
+
+  val input: List[Command] = Source
+    .fromResource("day07.txt")
+    .getLines()
+    .flatMap(Command.parser.parse(_).toOption.map(_._2))
+    .toList
+
+  def sizeOf(fse: FSE): BigInt =
+    fse match
+      case FSE.File(_, size) => BigInt(size)
+      case FSE.Folder(_, contents) =>
+        contents.map(sizeOf).sum
+
+  def collectUpTo(fse: FSE, size: BigInt = BigInt(100000)): Vector[BigInt] = {
+    def recur(fse: FSE): Vector[BigInt] =
+      fse match
+        case FSE.File(_, _) => Vector.empty
+        case folder @ FSE.Folder(_, contents) =>
+          val folderSize = sizeOf(folder)
+
+          val subSizes = contents
+            .flatMap(recur)
+          if folderSize <= size then folderSize +: subSizes
+          else subSizes
+
+    recur(fse)
+  }
+
+  @main
+  def solution1(): Unit =
+    input
+      .pipe(addViaCommands)
+      .pipe(collectUpTo(_))
+      .pipe(_.sum)
+      .pipe(pprint.log(_))
+
+}

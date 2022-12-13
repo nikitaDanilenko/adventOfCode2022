d627106e9f0a2c4d453762d59120623ed6556314
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-12-13 17:14:48 +0100

    Add solution for day 13.2

--- src/main/scala/day13/Day13.scala
+++ src/main/scala/day13/Day13.scala
@@ -4,6 +4,7 @@ import scala.io.Source
 import utils.CollectionUtil.syntax._
 import cats.syntax.order._
 import scala.util.chaining._
+import spire.compat._
 
 object Day13 {
 
@@ -26,5 +27,18 @@ object Day13 {
       .collect { case ((e1, e2), i) if e1 <= e2 => 1 + i }
       .sum
       .pipe(pprint.log(_))
+
+  @main
+  def solution2(): Unit =
+    val divider1 = Element.Multiple(List(Element.Multiple(List(Element.Single(2)))))
+    val divider2 = Element.Multiple(List(Element.Multiple(List(Element.Single(6)))))
+    val allElements = List(divider1, divider2) ++ Source
+      .fromResource("real/day13.txt")
+      .getLines()
+      .filter(_.nonEmpty)
+      .flatMap(io.circe.parser.decode[Element](_).toOption)
+    val sorted = allElements.sorted
+    val result = (1 + sorted.indexOf(divider1)) * (1 + sorted.indexOf(divider2))
+    pprint.log(result)
 
 }

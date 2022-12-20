41708704f1294b99efc39259e6c4de6484b68062
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-12-20 23:45:16 +0100

    Add solution for day 20.1

---
+++ src/main/scala/day20/Day20.scala
@@ -0,0 +1,58 @@
+package day20
+
+import scala.io.Source
+
+object Day20 {
+
+  val input: List[Int] = Source
+    .fromResource("test/day20.txt")
+    .getLines()
+    .map(_.toInt)
+    .toList
+
+  def insertAtIndex[A](index: Int, value: A, list: List[A]): List[A] =
+    val (beforeIndex, afterEqIndex) = list.splitAt(index)
+    beforeIndex ++ (value +: afterEqIndex)
+
+  def wrapIndex(positionalIndex: Int, listSize: Int, value: Long): Int =
+    val mod = listSize - 1
+    val movedPositional = ((positionalIndex + value) % mod).intValue()
+    if movedPositional < 0 then movedPositional + mod
+    else if movedPositional >= listSize then movedPositional - mod
+    else movedPositional
+
+  def moveValue(value: Long, index: Int, list: List[(Long, Int)], listSize: Int): List[(Long, Int)] =
+    val positionalIndex = list.indexWhere(_._2 == index)
+    val listWithoutIndex = list.filter(p => p._2 != index)
+    val newIndex = wrapIndex(positionalIndex, listSize, value)
+    insertAtIndex(newIndex, (value, index), listWithoutIndex)
+
+  def iteratedMove(initial: List[Long], listSize: Int): List[Long] =
+    val indexedInitial = initial.zipWithIndex
+    indexedInitial
+      .foldLeft(indexedInitial) { case (moved, (value, index)) =>
+        moveValue(value, index, moved, listSize)
+      }
+      .map(_._1)
+
+  def findCoordinates(list: List[Long], listSize: Int): Long =
+    val zeroIndex = list.indexOf(0)
+    List(1000, 2000, 3000).map { offset =>
+      list((offset + zeroIndex) % listSize)
+    }.sum
+
+  @main
+  def solution1(): Unit =
+    val initial = input.map(_.toLong)
+    val size = initial.size
+    pprint.log(findCoordinates(iteratedMove(initial, size), size))
+
+//  @main
+//  def solution2(): Unit =
+//    val initial = input.map(_ * 811589153L)
+//    val size = initial.size
+//    val mixed = 1.to(10).foldLeft(initial)((xs, _) => iteratedMove(xs, size))
+//    val result = findCoordinates(mixed, size)
+//    pprint.log(result)
+
+}
